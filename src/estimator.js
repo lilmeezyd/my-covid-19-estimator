4b5a3467bc4a1255973075a9588e178d9b13e53d
Denis Moini <denismoini@yahoo.com>
2020-04-09 20:56:41 +0300

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,23 @@
 const covid19ImpactEstimator = (data) => {
+
+  const timeNormalize = (periodType) => {
+  	let { timeToElapse } = data;
+  	if (periodType === 'days') {
+  		timeToElapse = timeToElapse * 1;
+  	} else if (periodType === 'weeks') {
+  		timeToElapse = timeToElapse * 7;
+  	} else {
+  		timeToElapse = timeToElapse * 30;
+  	}
+
+  	return timeToElapse;
+  };
+
   const impact = {};
   const severeImpact = {};
-  const periodType = data.periodType;
-  const timeToElapse = timeNormalize(periodType);
-  const factor = Math.trunc(timeToElapse / 3);
+  const { periodType } = periodType;
+  const periodTime = timeNormalize(periodType);
+  const factor = Math.trunc(periodTime / 3);
 
   impact.currentlyInfected = data.reportedCases * 10;
   impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** factor;
@@ -13,16 +27,5 @@ const covid19ImpactEstimator = (data) => {
 
   return { data, impact, severeImpact };
 };
-
-const timeNormalize = (periodType) => {
-  if (periodType === 'days') {
-  	timeToElapse = data.timeToElapse;
-  } else if (periodType === 'weeks') {
-  	timeToElapse = data.timeToElapse * 7;
-  } else {
-  	timeToElapse = data.timeToElapse * 30;
-  }
-  return timeToElapse;
-  }
 
 export default covid19ImpactEstimator;
