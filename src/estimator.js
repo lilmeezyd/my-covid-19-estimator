f1d79fdaac23e60bbcacbe040c3f894941e3dc1f
Denis Moini <denismoini@yahoo.com>
2020-04-09 15:54:54 +0300

    challange 1.0

--- src/estimator.js
+++ src/estimator.js
@@ -1,15 +1,16 @@
 const covid19ImpactEstimator = (data) => {
-	const impact = {};
-	const severeImpact = {};
-	const factor = Math.trunc(data.timeToElapse/3);
 
-	impact.currentlyInfected = data.reportedCases * 10;
-	impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** factor;
+  const impact = {};
+  const severeImpact = {};
+  const factor = Math.trunc(data.timeToElapse / 3);
 
-	severeImpact.currentlyInfected = data.reportedCases * 50;
-	severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 2 ** factor;
+  impact.currentlyInfected = data.reportedCases * 10;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** factor;
 
-	return { data, impact, severeImpact }
-}
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 2 ** factor;
+
+  return { data, impact, severeImpact };
+};
 
 export default covid19ImpactEstimator;
